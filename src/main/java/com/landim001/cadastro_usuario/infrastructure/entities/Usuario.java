e6b806ab8c9b0da7f1226fa1871e3574a3d2ed0c
landim01 <aclfrancisco@minha.fag.edu.br>
2025-11-26 07:54:24 -0300

    Initial commit

---
+++ src/main/java/com/landim001/cadastro_usuario/infrastructure/entities/Usuario.java
@@ -0,0 +1,29 @@
+package com.landim001.cadastro_usuario.infrastructure.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+
+}
